344fe25fd4e7294bc06ca75b53d3c5afc521a961
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-10-26 17:04:39 -0300

    add chat

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
+const chatSocket = require('./src/sockets/chat');
 
 const io = require('socket.io')(http, {
   cors: {
     origin: 'http://localhost:3000', // url aceita pelo cors
     methods: ['GET', 'POST'], // Métodos aceitos pela url
-  }});
-
-io.on('connection', (socket) => {
-  console.log(`Usuário conectado. ID: ${socket.id} `);
-
-  socket.on('ping', () => {
-    console.log(`${socket.id} emitiu um ping!`);
-    io.emit('pong', `${socket.id} enviou um ping!`);
-  });
-
-  socket.emit('ola',
-    'Que bom que você chegou aqui! Fica mais um cadin, vai ter bolo :)'
-  );
-
+  },
 });
 
+app.use(express.static(__dirname + '/public'));
+
+chatSocket(io);
+
 app.get('/', (_req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 http.listen(3000, () => {
---
+++ src/sockets/ping.js
@@ -0,0 +1,12 @@
+module.exports = (io) => {
+  io.on('connection', (socket) => {
+
+    socket.emit('ola', 'Que bom que você chegou aqui! Fica mais um cadin, vai ter bolo :)');
+
+    socket.on('ping', () => {
+      console.log(`${socket.id} emitiu um ping!`);
+      io.emit('pong', `${socket.id} enviou um ping!, receba um pong`); 
+      // essa linha envia um aviso para o cliente que o ping chegou.
+    });
+  });
+};
